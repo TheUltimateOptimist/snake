11fe34ba28cecca3f20e9d0d941fe767f7911ad1
TheUltimateOptimist <digitalConfidence@web.de>
2022-04-05 22:48:19 +0200

    added GameBoard class

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -1,71 +1,15 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-
-
-class InvalidKeyException implements Exception{
-
-}
-
-class DirectionFactory{
-  static Direction fromKeyLabel(String keyLabel){
-    switch(keyLabel){
-      case "Arrow Right": return Direction.right;
-      case "Arrow Left": return Direction.left;
-      case "Arrow Up": return Direction.up;
-      case "Arrow Down": return Direction.down;
-    }
-    throw InvalidKeyException();
-  }
-}
-
-enum Direction {
-  left,
-  right,
-  up,
-  down,
-}
-
 class GameBoard {
-  int velocity;
-  int width;
-  int height;
-  int leftDistance;
-  int bottomDistance;
-  Direction direction;
+  final int verticalSquares;
+  final int horizontalSquares;
+  final int sideLength;
 
   GameBoard({
-    this.width = 200,
-    this.height = 200,
-    this.velocity = 5,
-    this.leftDistance = 100,
-    this.bottomDistance = 100,
-    this.direction = Direction.right,
+    this.verticalSquares = 15,
+    this.horizontalSquares = 15,
+    this.sideLength = 15,
   });
 
-  void move() {
-    switch (direction) {
-      case Direction.left:
-        leftDistance -= velocity;
-        break;
-      case Direction.right:
-        leftDistance += velocity;
-        break;
-      case Direction.up:
-        bottomDistance += velocity;
-        break;
-      case Direction.down:
-        bottomDistance -= velocity;
-        break;
-    }
-  }
+  int get width => horizontalSquares*sideLength;
 
-  bool hasTouchedBorder() {
-    if (bottomDistance >= height) return true;
-    if (leftDistance >= width) return true;
-    if (leftDistance <= 0) return true;
-    if (bottomDistance <= 0) return true;
-    return false;
-  }
-
-
+  int get height => verticalSquares*sideLength;
 }
