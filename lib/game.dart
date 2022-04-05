cebb1cb0457fa15fd68279ca062743ff8153e1dd
TheUltimateOptimist <digitalConfidence@web.de>
2022-04-05 22:06:11 +0200

    added game interface

---
+++ lib/game.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:snake/direction.dart';
+
+class Game{
+  void changeDirection(String keyLabel){
+    final direction = DirectionFactory().createWithKeyLabel(keyLabel);
+    //TODO: make direction change
+  }
+
+  bool canMoveForward(){
+    //TODO: implement canMoveForward
+    throw UnimplementedError();
+  }
+
+  int getBoardWidth(){
+    //TODO: implement getBoardWidth
+    throw UnimplementedError();
+  }
+
+  int getBoardHeight(){
+    //TODO: implement getBoardHeight
+    throw UnimplementedError();
+  }
+
+  List<Container> getContainers(){
+    //TODO: implement getContainers
+    throw UnimplementedError();
+  }
+}
