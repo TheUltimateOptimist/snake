98b7c815d2298174a83dd2949570e436f13a147e
TheUltimateOptimist <digitalConfidence@web.de>
2022-04-05 22:59:49 +0200

    added GameBoard reference to position class renamed it to BoardPosition and added isNotInBoard method

--- lib/position.dart
+++ lib/position.dart
@@ -1,7 +1,23 @@
-class Position{
-  Position(this.leftDistance, this.bottomDistance);
+import 'game_board.dart';
+
+class BoardPosition {
+  BoardPosition(
+    this.leftDistance,
+    this.bottomDistance,
+    this.board,
+  );
 
   int leftDistance;
-  
+
   int bottomDistance;
+
+  final GameBoard board;
+
+  bool isNotInBoard() {
+    if (bottomDistance >= board.height) return true;
+    if (leftDistance >= board.width) return true;
+    if (leftDistance <= 0) return true;
+    if (bottomDistance <= 0) return true;
+    return false;
+  }
 }
