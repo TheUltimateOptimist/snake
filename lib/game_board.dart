6beac21f65f42cf48909ea70103d3bbc66e4c89a
TheUltimateOptimist <digitalConfidence@web.de>
2022-04-06 15:57:10 +0200

    added snackPosition and shuffleSnackPosition to GameBoard class

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -1,15 +1,25 @@
+import 'package:snake/position.dart';
+
 class GameBoard {
   final int verticalSquares;
   final int horizontalSquares;
   final int sideLength;
+  final BoardPosition snackPosition;
 
   GameBoard({
     this.verticalSquares = 15,
     this.horizontalSquares = 15,
     this.sideLength = 15,
+    required this.snackPosition
   });
 
   int get width => horizontalSquares*sideLength;
 
   int get height => verticalSquares*sideLength;
+
+  void shuffleSnackPosition(){
+    //TODO: implement shuffleSnackPosition
+  }
+
+
 }
