648329dec90f39f4571df7f06afef164c03d72ec
TheUltimateOptimist <digitalConfidence@web.de>
2022-04-08 00:54:28 +0200

    added SnakeLine class

---
+++ lib/snake_line.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:snake/snake_path.dart';
+
+class SnakeLine extends SnakePath{
+  const SnakeLine(Offset start, Offset end) : super(start, end);
+
+  @override
+  bool isLongerThan(double remainingLength) {
+    return _getLength() > remainingLength;
+  }
+
+  double _getLength(){
+    return (start.dx - end.dx).abs() + (start.dy - end.dy).abs();
+  }
+
+  @override
+  Path toCompletePath() {
+    return Path()..moveTo(start.dx, start.dy)..lineTo(end.dx, end.dy);
+  }
+
+  @override
+  Path toInCompletePath(double remainingLength) {
+    final vectorX = end.dx - start.dx;
+    final vectorY = end.dy - start.dy;
+    return Path()..moveTo(start.dx, start.dy)..lineTo(start.dx + vectorX*remainingLength, start.dy + vectorY*remainingLength);
+  }
+}
