a33633b0257eed7906730938e6045fc03f688b15
TheUltimateOptimist <digitalConfidence@web.de>
2022-04-09 02:13:47 +0200

    added isOutOfBoard method to OffsetExtension and thereby removed BoardPosition class

--- lib/offset_extension.dart
+++ lib/offset_extension.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:snake/direction.dart';
+import 'package:snake/game_board.dart';
 
 extension OffsetExtension on Offset{
   Offset moveForward(double distance, Direction direction){
@@ -14,4 +16,13 @@ extension OffsetExtension on Offset{
         return Offset(dx, dy + distance);
         }
     }
+
+  bool isOutOfBoard(GameBoard gameBoard) {
+    final boxHalf = gameBoard.sideLength/2;
+    if (dx > gameBoard.width - boxHalf) return true;
+    if (dy > gameBoard.height - boxHalf) return true;
+    if (dx < boxHalf) return true;
+    if (dy < boxHalf) return true;
+    return false;
+  }
 }
