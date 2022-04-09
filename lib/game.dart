c4c5085d473e6dace5e75238c28e6c2d14302079
TheUltimateOptimist <digitalConfidence@web.de>
2022-04-09 02:42:32 +0200

    added Game.defaultOption factory constructor and used in main.dart

--- lib/game.dart
+++ lib/game.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:snake/direction.dart';
+import 'package:snake/game_board.dart';
+
+import 'snake.dart';
 
 class Game{
+  Game(this.gameBoard, this.snake);
+
+  factory Game.defaultOption(){
+    GameBoard gameBoard = GameBoard();
+    final thickness = gameBoard.sideLength.toDouble();
+    Snake snake = Snake([gameBoard.getInitialSnakePath(2*thickness)], thickness);
+    return Game(gameBoard, snake);
+  }
+
+  final GameBoard gameBoard;
+
+  final Snake snake;
   void changeDirection(String keyLabel){
     final direction = DirectionFactory().createWithKeyLabel(keyLabel);
     //TODO: make direction change
--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
 
 @override
   void initState() {
-    game = Game();
+    game = Game.defaultOption();
     ticker = createTicker((elapsed) {
       if(game.canMoveForward()){
         game.moveForward(elapsed);
